eea74437306ad6003136adb0cf2e6e05afbc5277
perlancar (on netbook-zenbook-ux305) <perlancar@gmail.com>
2019-02-14 21:43:51 +0700

    Move data to __DATA__

--- lib/Calendar/Dates/FRM.pm
+++ lib/Calendar/Dates/FRM.pm
@@ -11,35 +11,6 @@ use Role::Tiny::With;
 
 with 'Calendar::DatesRoles::FromData';
 
-our @ENTRIES = (
-    # nov2018exam
-    {date=>'2019-01-03', summary=>'Exam results sent via email', tags=>['nov2018exam']},
-
-    # may2019exam
-    {date=>'2018-12-01', summary=>'Early registration opened', tags=>['may2019exam']},
-    {date=>'2019-01-31', summary=>'Early registration closed', tags=>['may2019exam']},
-    {date=>'2019-02-01', summary=>'Standard registration opened', tags=>['may2019exam']},
-    {date=>'2019-02-28', summary=>'Standard registration closed', tags=>['may2019exam']},
-    {date=>'2019-03-01', summary=>'Late registration opened', tags=>['may2019exam']},
-    {date=>'2019-04-15', summary=>'Late registration closed', tags=>['may2019exam']},
-    {date=>'2019-04-15', summary=>'Defer deadline', tags=>['may2019exam']},
-    {date=>'2019-05-01', summary=>'Admission tickets released', tags=>['may2019exam']},
-    {date=>'2019-05-18', summary=>'Exam day', tags=>['may2019exam']},
-    {date=>'2019-06-28', summary=>'Exam results sent via email', tags=>['may2019exam']},
-
-    # nov2019exam
-    {date=>'2019-05-01', summary=>'Early registration opened', tags=>['nov2019exam']},
-    {date=>'2019-07-31', summary=>'Early registration closed', tags=>['nov2019exam']},
-    {date=>'2019-08-01', summary=>'Standard registration opened', tags=>['nov2019exam']},
-    {date=>'2019-08-31', summary=>'Standard registration closed', tags=>['nov2019exam']},
-    {date=>'2019-09-01', summary=>'Late registration opened', tags=>['nov2019exam']},
-    {date=>'2019-10-15', summary=>'Late registration closed', tags=>['nov2019exam']},
-    {date=>'2019-10-15', summary=>'Defer deadline', tags=>['nov2019exam']},
-    {date=>'2019-11-01', summary=>'Admission tickets released', tags=>['nov2019exam']},
-    {date=>'2019-11-16', summary=>'Exam day', tags=>['nov2019exam']},
-    {date=>'2020-01-02', summary=>'Exam results sent via email', tags=>['nov2019exam']},
-);
-
 1;
 # ABSTRACT: FRM exam calendar
 
@@ -53,3 +24,33 @@ This module provides FRM exam calendar using the L<Calendar::Dates> interface.
 L<https://www.garp.org/#!/frm/program-exams>
 
 L<https://en.wikipedia.org/wiki/Financial_risk_management>
+
+=cut
+
+__DATA__
+# nov2018exam
+2019-01-03;Exam results sent via email;nov2018exam
+
+# may2019exam
+2018-12-01;Early registration opened;may2019exam
+2019-01-31;Early registration closed;may2019exam
+2019-02-01;Standard registration opened;may2019exam
+2019-02-28;Standard registration closed;may2019exam
+2019-03-01;Late registration opened;may2019exam
+2019-04-15;Late registration closed;may2019exam
+2019-04-15;Defer deadline;may2019exam
+2019-05-01;Admission tickets released;may2019exam
+2019-05-18;Exam day;may2019exam
+2019-06-28;Exam results sent via email;may2019exam
+
+# nov2019exam
+2019-05-01;Early registration opened;nov2019exam
+2019-07-31;Early registration closed;nov2019exam
+2019-08-01;Standard registration opened;nov2019exam
+2019-08-31;Standard registration closed;nov2019exam
+2019-09-01;Late registration opened;nov2019exam
+2019-10-15;Late registration closed;nov2019exam
+2019-10-15;Defer deadline;nov2019exam
+2019-11-01;Admission tickets released;nov2019exam
+2019-11-16;Exam day;nov2019exam
+2020-01-02;Exam results sent via email;nov2019exam
